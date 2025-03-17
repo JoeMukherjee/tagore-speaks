318c30facaa664d0bad9f0388530b861c14d36ae
Abhijeet Agnihotri <abhijeet.agni.1993@gmail.com>
2025-03-17 13:52:27 -0400

    updated the new svg/png files of tagore

--- tagore-frontend/src/components/Header.tsx
+++ tagore-frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 // src/components/Header.tsx
 import React from "react";
-import tagoreImage from "../assets/tagore_speaks.png";
+import tagoreImage from "../assets/tagore.svg";
 import ThemeToggle from "./ThemeToggle";
 import ExportPdfButton from "./Chat/ExportPdfButton";
 import { useTheme } from "../theme/useTheme";
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
 
     return (
         <header
-            className="h-20 fixed top-0 left-0 right-0 z-10 flex items-center px-4"
+            className="h-20 fixed pt-2 top-0 left-0 right-0 z-10 flex items-center px-4"
             style={{ backgroundColor: theme.colors.background.DEFAULT }}
         >
             {/* Left side - Export button */}
@@ -20,7 +20,14 @@ const Header: React.FC = () => {
 
             {/* Center - Logo and text */}
             <div className="flex-1 flex justify-center items-center">
-                <img src={tagoreImage} alt="Tagore" className="h-20" />
+                <img
+                    src={tagoreImage}
+                    alt="Tagore"
+                    className="h-20"
+                    style={{
+                        filter: "drop-shadow(0 0 5px rgba(253 255 223 / 0.8))",
+                    }}
+                />
                 <div className="ml-3 flex flex-col justify-start">
                     <span className="text-2xl text-left font-heading font-bold leading-tight text-primary">
                         Tagore
